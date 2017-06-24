e9c35bae6ab5da69284752067b887503d7b2d05f
maggytheirish <kostinam@student.hu-berlin.de>
2017-06-24 12:11:41 +0200

    Update Data Exploration.R

--- Data Exploration/Data Exploration.R
+++ Data Exploration/Data Exploration.R
@@ -8,17 +8,17 @@ library(caret)
 
 # Loading the train and test datasets
 
-train<-read.csv("train.csv")
-store<-read.csv("store.csv")
+train=read.csv("train.csv")
+store=read.csv("store.csv")
 
 # Plot the dependent variable
 
-train_sales <- subset(train, Sales != 0, drop = TRUE)
+train_sales = subset(train, Sales != 0, drop = TRUE)
 hist(aggregate(train_sales$Sales, 
                by = list(train_sales$Store), mean)$x, 100, 
      main = "Mean sales per store when store was not closed")
 hist(train$Sales, 100)
-train_sales <- subset(train, Sales != 0, drop = TRUE)
+train_sales = subset(train, Sales != 0, drop = TRUE)
 hist(aggregate(train_sales$Customers, 
                by = list(train_sales$Store), mean)$x, 100, 
      main = "Mean sales per store when store was not closed")

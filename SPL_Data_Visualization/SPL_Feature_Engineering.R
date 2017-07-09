28f094631d3ce650b0a6195908d8132cab598dd5
Jay-iyer <choodamj@hu-berlin.de>
2017-07-09 18:53:17 +0200

    Update SPL_Feature_Engineering.R

--- SPL_Data_Visualization/SPL_Feature_Engineering.R
+++ SPL_Data_Visualization/SPL_Feature_Engineering.R
@@ -48,61 +48,61 @@ train$CompetitionSinceDate[is.na(train$CompetitionSinceDate)] = mean(train$Compe
 train$Promo2SinceDate[is.na(train$Promo2SinceDate)] = mean(train$Promo2SinceDate, na.rm = TRUE)
 summary(train)   
 
-# Adding features with mean sales 
-
-train$Store <- as.factor(train$Store)
-train$Date<-as.Date(paste(train$Date,sep="-"),format= "%Y-%m-%d")
-train <- separate(train, Date, into = c("Year", "Month","Day"), sep="-")  #splitting the date into separate colums
-
-features_avg <- setNames(aggregate(train$Sales, list(train$Store), mean),c("Store","AvgSalesPerStore"))
-features_avg$AvgVisitsPerStore <- aggregate(train$Customers, list(train$Store), mean)[,2]
-
-features_dow<- setNames(aggregate(train$Sales, list(train$Store,train$DayOfWeek), mean),c("Store","DayOfWeek","AvgSalesPerStorePerDayOfWeek"))
-features_dow$AvgVisitsPerStorePerDayOfWeek<- aggregate(train$Customers, list(train$Store,train$DayOfWeek), mean)[,3]
-
-features_year <- setNames(aggregate(train$Sales, list(train$Store,train$Year), mean),c("Store","Year","AvgSalesPerStorePerYear"))
-features_year$AvgVisitsPerStorePerYear <- aggregate(train$Customers, list(train$Store,train$Year), mean)[,3]
-
-features_mon<- setNames(aggregate(train$Sales, list(train$Store,train$Year,train$Month), mean),c("Store","Year","Month","AvgSalesPerStorePerMonth"))
-features_mon$AvgVisitsPerStorePerMonth<- aggregate(train$Customers, list(train$Store,train$Year,train$Month), mean)[,4]
-
-#Merging new features with the dataset
-train<-merge(train, features_avg, by="Store")
-train<-merge(train, features_dow, by=c("Store","DayOfWeek"))
-train<-merge(train, features_year, by=c("Store","Year"))
-train<-merge(train, features_mon, by=c("Store","Year","Month"))
-
 # Creating separate test set where sales need to be predicted
 train$NewDate = as.Date(paste(train$NewDate,sep="-"),format= "%Y-%m-%d")
 testdate = train$NewDate>="2015-07-20"  # Selecting the last 10 days for forecasting
-newdata_set = train[testdate==T, ]
+newdata.set = train[testdate==T, ]
 train = train[testdate==F, ]  # remove test observations
+newdata.set$NewDate = NULL
+
+# Adding features with mean sales 
+
+train$Store = as.factor(train$Store)
+train$Date = as.Date(paste(train$Date,sep="-"),format= "%Y-%m-%d")
+train = separate(train, Date, into = c("Year", "Month","Day"), sep="-")  #splitting the date into separate colums
+
+features.avg = setNames(aggregate(train$Sales, list(train$Store), mean),c("Store","AvgSalesPerStore"))
+features.avg$AvgVisitsPerStore = aggregate(train$Customers, list(train$Store), mean)[,2]
+
+features.dow = setNames(aggregate(train$Sales, list(train$Store,train$DayOfWeek), mean),c("Store","DayOfWeek","AvgSalesPerStorePerDayOfWeek"))
+features.dow$AvgVisitsPerStorePerDayOfWeek = aggregate(train$Customers, list(train$Store,train$DayOfWeek), mean)[,3]
+
+features.year = setNames(aggregate(train$Sales, list(train$Store,train$Year), mean),c("Store","Year","AvgSalesPerStorePerYear"))
+features.year$AvgVisitsPerStorePerYear = aggregate(train$Customers, list(train$Store,train$Year), mean)[,3]
+
+features.mon = setNames(aggregate(train$Sales, list(train$Store,train$Year,train$Month), mean),c("Store","Year","Month","AvgSalesPerStorePerMonth"))
+features.mon$AvgVisitsPerStorePerMonth = aggregate(train$Customers, list(train$Store,train$Year,train$Month), mean)[,4]
+
+#Merging new features with the dataset
+train = merge(train, features.avg, by="Store")
+train = merge(train, features.dow, by=c("Store","DayOfWeek"))
+train = merge(train, features.year, by=c("Store","Year"))
+train = merge(train, features.mon, by=c("Store","Year","Month"))
 
 # Creating train dataset
 train$NewDate = as.factor(train$NewDate)
 set.seed(123)
 idx = createDataPartition(train$NewDate, p=0.05, list = F)
-train_set = train[idx, ]
-train_set$NewDate = NULL
-newdata_set$NewDate = NULL
+train.set = train[idx, ]
+train.set$NewDate = NULL
 
 # Checking the datasets
-summary(train_set)
-summary(newdata_set)
+summary(train.set)
+summary(newdata.set)
 
 # Remove the variable Sales that needs to be predicted from the testing dataset
-newdata_set$Sales = NULL
+newdata.set$Sales = NULL
 
 # Remove the variable customers related to Sales from both traiing and testing datasets
-newdata_set$Customers = NULL 
-train_set$Customers = NULL
+newdata.set$Customers = NULL 
+train.set$Customers = NULL
 
 # Create testing and training subsets within the train set
-idx.test = createDataPartition(train_set$Sales,p=0.8,list=F)
-train_final = train_set[idx.test, ]
-test_final = train_set[-idx.test, ]
+idx.test = createDataPartition(train.set$Sales,p=0.8,list=F)
+train.final = train.set[idx.test, ]
+test.final = train.set[-idx.test, ]
 
 # Saving datasets
-saveRDS(train_final,"train.b1.v3")
-saveRDS(test_final,"test.b1.v3")
-saveRDS(newdata_set,"class.b1.v3")
+saveRDS(train.final,"train")
+saveRDS(test.final,"test")
+saveRDS(newdata.set,"class")

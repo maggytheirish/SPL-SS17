baba0b6d68e5c3bc668a510ee6bd9b1e4c178199
Jay-iyer <jaya1992@gmail.com>
2017-08-09 17:36:30 +0200

    added wrapper for model evaluation

---
+++ wrapper.R
@@ -0,0 +1,101 @@
+#input - dataset for prediction, model 
+
+evaluate <- function(model,data,actual){
+  
+  type = readline(prompt = "Choose - Classification or Regression")
+  
+  if(type == "Classification"){
+    
+    classifier = readline(prompt = "Type in the name of the classifier : (eg. lr, xgb)")
+    
+    if(classifier=="lr"){
+      probabilities <- predict(model,newdata= data)
+    }else {probabilities <- predict(model,newdata= data)}
+    
+    stats <- summary(probabilities)
+    print("Descriptive statistics for the prediction probabilities : ")
+    print(stats)
+    
+    # Classifying using different threshold values
+    
+    tgt <- readline(prompt = " Is the target variable numeric? It needs to be labelled for
+                    models other than lr")
+    pos <- readline(prompt = "Enter positive case of the factor variable")
+    neg <- readline(prompt = "Enter negative case of the factor variable")
+
+      print("Classification using mean, median, 3rd quadrant values of the probabilities as thresholds")
+      
+      pred_random <- ifelse(probabilities>=0.5,pos,neg)
+      pred_mean <- ifelse(probabilities>=stats[3],pos,neg) 
+      pred_median <- ifelse(probabilities>=stats[4],pos,neg)
+      pred_3quad <- ifelse(probabilities>=stats[5],pos,neg)
+      
+      threshold <- readline(prompt = "Do you want to use a specific threshold for classification?
+                      Type in the numerical threshold value if not type 'no'")
+      if(threshold=="no"){
+        print("You have chosen to use a default threshold")
+      } else { pred_user <- ifelse(probabilities>=as.numeric(threshold),pos,neg)}
+      
+      # Display results
+      print("Threshold - Mean")
+      print(table(pred_mean))
+      
+  }else { if(type == "Regression") {
+    
+    # Predictions
+    pred <- predict(model,data)
+    
+    print("The descriptive statistics of the predicted values")
+    print(summary(pred))
+    print("The descriptive statistics of the actual values")
+    print(summary(actual))
+    
+    results <- cbind(actual,pred)
+    #View(results)
+    
+    # Metric 
+    metric <- readline(prompt = "Which metric would you like to use for error analysis?
+                       Choose btw rmse,user. If you want to use a specific metric please
+                       add it to the helper function and name it as user.")
+    
+    if(metric=="rmse"){
+      #error <- rmse(actual,pred) add rmse to helper function
+      rmse <- sqrt(mean((actual-pred)^2))
+      print(paste0("The prediction error is :",rmse))
+    }else{ error <- user(actual,pred)}
+    
+    
+    # Error decomposition
+    mse <- mean((actual-pred)^2)
+    abs.error <- sum((actual-pred)^2)
+    bias <- mean(actual) - mean(pred)
+    percent.bias <- (rmse-bias)/rmse 
+    diff.skew <- skewness(actual) - skewness(pred)
+    
+    # Saving results 
+    
+    error_matrix <- cbind(rmse,mse,abs.error,bias,percent.bias,diff.skew)
+    #View(error_matrix)
+    
+    # Visualization
+    
+    x1 = actual
+    y1 = dnorm(actual,mean = mean(actual),sd=sd(actual))
+    
+    x2 = pred
+    y2 = dnorm(pred,mean = mean(pred),sd=sd(pred))
+    
+    print("Plotting the distributions of the actual and predicted values - green actual
+          red predicted")
+    
+    dist.plot <- plot(x1,y1,col="green", xlim=range(c(x1,x2)),ylim=range(c(y1,y2)))
+    points(x2,y2,col="red")
+    
+    dist.plot
+   
+    
+    
+    
+  }}
+
+}

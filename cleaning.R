e952d0478957531e62f7ea8656b190406a8875a6
maggytheirish <kostinam@student.hu-berlin.de>
2017-08-12 17:48:58 +0200

    Change in source file

--- cleaning.R
+++ cleaning.R
@@ -6,7 +6,7 @@
 
 # Load all required packages
 
-source("HelperFunctions.R")
+source("Helper_Functions.R")
 
 list.of.packages = c("rpart","lubridate","outliers","rpart.plot", "xgboost",
                      "caret","caretEnsemble", "randomForest","e1071","pROC","tidyr",
